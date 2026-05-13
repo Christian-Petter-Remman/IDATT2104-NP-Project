706b682ebce016f074ffbcaf799ef7e148439e54
Christian Petter Remman <cpremman@gmail.com>
2026-05-13 15:24:25 +0200

    feat: derive Clone for PNCounter struct to enable cloning functionality

--- crdt-core/src/counters/pn_counter.rs
+++ crdt-core/src/counters/pn_counter.rs
@@ -6,6 +6,7 @@ use super::g_counter::GCounter;
 /// Two GCounters: one for increments, one for decrements.
 /// Value = increments.value() - decrements.value() (may be negative).
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone)]
 pub struct PNCounter {
     increments: GCounter,
     decrements: GCounter,
